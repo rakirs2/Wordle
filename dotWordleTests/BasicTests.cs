5b4fd0c3bd7ea79681a0078415597d0953586630
rakirs2 <srmylavarapu@microsoft.com>
2023-09-22 18:43:49 -0500

    cleaning up

--- dotWordleTests/BasicTests.cs
+++ dotWordleTests/BasicTests.cs
@@ -1,57 +1,55 @@
-using System.Reflection.Metadata;
-
 namespace dotWordleTests;
 
 [TestClass]
 public class BasicTests
 {
-    private readonly EasyWordleBot wordleBot = new();
+    private readonly EasyWordleBot _wordleBot = new();
 
     [TestMethod]
     public void TestInitialization()
     {
-        wordleBot.Should().NotBeNull();
-        wordleBot.GetRemainingWords().Count.Should().Be(Constants.TotalNumberOfValidGuesses);
-        wordleBot.GetGuessNumber().Should().Be(1);
-        wordleBot.HasWon().Should().BeFalse();
-        wordleBot.GetGuessesRemaining().Should().Be(6);
+        _wordleBot.Should().NotBeNull();
+        _wordleBot.GetRemainingWords().Count.Should().Be(Constants.TotalNumberOfValidGuesses);
+        _wordleBot.GetGuessNumber().Should().Be(1);
+        _wordleBot.HasWon().Should().BeFalse();
+        _wordleBot.GetGuessesRemaining().Should().Be(6);
     }
 
     [TestMethod]
     public void InvalidGuess()
     {
-        wordleBot.GuessWord(Constants.DefaultInvalidGuess).isValidGuess.Should().BeFalse();
+        _wordleBot.GuessWord(Constants.DefaultInvalidGuess).isValidGuess.Should().BeFalse();
     }
 
     [TestMethod]
     public void ValidGuess()
     {
-        wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
+        _wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
     }
 
     [TestMethod]
     public void ValidGuessIncrementsGuessesRemainingByOne()
     {
-        wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
-        wordleBot.GetGuessesRemaining().Should().Be(5);
+        _wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
+        _wordleBot.GetGuessesRemaining().Should().Be(5);
     }
 
     [TestMethod]
     public void ValidGuessIncrementsSixTimes()
     {
-        wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
-        wordleBot.GetGuessesRemaining().Should().Be(5);
-        wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
-        wordleBot.GetGuessesRemaining().Should().Be(4);
-        wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
-        wordleBot.GetGuessesRemaining().Should().Be(3);
-        wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
-        wordleBot.GetGuessesRemaining().Should().Be(2);
-        wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
-        wordleBot.GetGuessesRemaining().Should().Be(1);
-        wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
-        wordleBot.GetGuessesRemaining().Should().Be(0);
-        wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeFalse();
-        wordleBot.GetGuessesRemaining().Should().Be(0);
+        _wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
+        _wordleBot.GetGuessesRemaining().Should().Be(5);
+        _wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
+        _wordleBot.GetGuessesRemaining().Should().Be(4);
+        _wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
+        _wordleBot.GetGuessesRemaining().Should().Be(3);
+        _wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
+        _wordleBot.GetGuessesRemaining().Should().Be(2);
+        _wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
+        _wordleBot.GetGuessesRemaining().Should().Be(1);
+        _wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeTrue();
+        _wordleBot.GetGuessesRemaining().Should().Be(0);
+        _wordleBot.GuessWord(Constants.DefaultValidGuess).isValidGuess.Should().BeFalse();
+        _wordleBot.GetGuessesRemaining().Should().Be(0);
     }
 }
