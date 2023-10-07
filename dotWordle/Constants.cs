8a31d0fe806c449daed3328e5740b9ce1ec127aa
Srikar Mylavarapu <srikarmylavarapu@gmail.com>
2023-10-06 22:20:41 -0500

    playing with constants

--- dotWordle/Constants.cs
+++ dotWordle/Constants.cs
@@ -1,14 +1,14 @@
 ﻿namespace dotWordle;
 
-internal class Constants
+internal static class Constants
 {
-    internal static uint TotalNumberOfGuesses = 6;
-    internal static uint NumberOfLetters = 5;
-    internal static int TotalNumberOfValidGuesses = 12974;
-    internal static string DefaultValidGuess = "slate";
-    internal static string DefaultInvalidGuess = "aaaaa";
-    internal static string DefaultWordForTest = "toxic";
-    internal static string DefaultValidFourGreens = "toxin";
-    internal static string DefaultValidOneYellow = "annex";
-    internal static List<char> DefaultGreens = new() { '0', '0', '0', '0', '0' };
+    internal static readonly uint TotalNumberOfGuesses = 6;
+    internal static readonly uint NumberOfLetters = 5;
+    internal static readonly int TotalNumberOfValidGuesses = 12974;
+    internal static  readonly string DefaultValidGuess = "slate";
+    internal static readonly string DefaultInvalidGuess = "aaaaa";
+    internal static readonly string DefaultWordForTest = "toxic";
+    internal static readonly string DefaultValidFourGreens = "toxin";
+    internal const string DefaultValidOneYellow = "annex";
+    internal static readonly List<char> DefaultGreens = new() { '0', '0', '0', '0', '0' };
 }
