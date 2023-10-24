289bfa1df0fddf9290cfc69f18a7c646dd8c7715
rakirs2 <srmylavarapu@microsoft.com>
2023-10-23 19:34:18 -0500

    found an error

---
+++ dotWordleTests/FunctionalityTests.cs
@@ -0,0 +1,27 @@
+namespace dotWordleTests;
+
+[TestClass]
+public class FunctionalityTests
+{
+    private ForcedWordleBot _forcedWordleBot = new(Constants.DefaultWordForTest);
+    private int remainingInitial;
+    [TestMethod]
+    public void TestInitialization()
+    {
+        _forcedWordleBot.Should().NotBeNull();
+        remainingInitial = _forcedWordleBot.GetRemainingWords().Count;
+    }
+
+    [TestMethod]
+    [Ignore]
+
+    public void ValidGuessIncrementsSixTimes()
+    {
+        _forcedWordleBot.GuessWord(Constants.DefaultValidGuess).IsValidGuess.Should().BeTrue();
+        // TODO: calculating wrong
+        var remainingAfter = _forcedWordleBot.GetRemainingWords().Count;
+        Assert.IsTrue(remainingAfter < remainingInitial);
+
+
+    }
+}
